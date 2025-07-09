94c1364ab9e1ffc577c185e60b6d4cfcc16e8098
BAmiriDev <126995732+BAmiriDev@users.noreply.github.com>
2025-07-09 11:18:56 +0100

    Initial commit

---
+++ src/main/java/com/example/spring_boot_blackjack_trainer/SpringBootBlackjackTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_blackjack_trainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlackjackTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlackjackTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_blackjack_trainer/SpringBootBlackjackTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_blackjack_trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlackjackTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
